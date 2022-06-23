75418c70fb291c61ae327703377347add6445254
Jonathan <jonathan@axonaut.com>
2022-06-23 08:36:35 +0200

    Initial commit

---
+++ src/main/java/com/example/testpictarine/TestPictarineApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpictarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPictarineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPictarineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpictarine/TestPictarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpictarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPictarineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
